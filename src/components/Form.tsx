bae3acd9e96548fc017bd14352272210bbfdd7e2
PauloFonseca <pfonseca.arq@gmail.com>
2024-02-05 16:50:12 -0300

    Crie um display para a validação da senha

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import Button from './Button';
-import validationData from '../utils/validationLogin';
+import { validationData } from '../utils/validationLogin';
 
 type FormProps = {
   responsibility: () => void;
@@ -19,14 +19,56 @@ function Form({ responsibility }: FormProps) {
     url: '',
   };
 
+  const passwordCheck = 'valid-password-check';
+  const passwordInvalid = 'invalid-password-check';
   const [dataLogin, setDataLogin] = useState(data);
   const [validBtn, setValidBtn] = useState(true);
+  const [characters, setCharacters] = useState(passwordInvalid);
+  const [amountCharacters, setAmountCharacters] = useState(passwordInvalid);
+  const [numbers, setNumbers] = useState(passwordInvalid);
+  const [special, setSpecial] = useState(passwordInvalid);
+  console.log(dataLogin.password.length);
+  const changeClassCharacter = () => {
+    if (dataLogin.password.length > 7) {
+      setCharacters(passwordCheck);
+    } else {
+      setCharacters(passwordInvalid);
+    }
+  };
+
+  const changeClassAmountCharacter = () => {
+    if (dataLogin.password.length > 0 && dataLogin.password.length <= 15) {
+      setAmountCharacters(passwordCheck);
+    } else {
+      setAmountCharacters(passwordInvalid);
+    }
+  };
+
+  const changeClassNumbers = () => {
+    if (dataLogin.password.match(/[a-zA-Z]/) && dataLogin.password.match(/[0-9]/)) {
+      setNumbers(passwordCheck);
+    } else {
+      setNumbers(passwordInvalid);
+    }
+  };
+
+  const changeClassSpecial = () => {
+    if (dataLogin.password.match(/[!@#$%^&*(),.?":{}|<>]/)) {
+      setSpecial(passwordCheck);
+    } else {
+      setSpecial(passwordInvalid);
+    }
+  };
 
   const handleChangeData = (e: React.ChangeEvent<HTMLInputElement>) => {
     setDataLogin((prevData) => ({
       ...prevData,
       [e.target.name]: e.target.value,
     }));
+    changeClassCharacter();
+    changeClassAmountCharacter();
+    changeClassNumbers();
+    changeClassSpecial();
     const validation = validationData(dataLogin);
     if (validation) {
       setValidBtn(false);
@@ -36,60 +78,69 @@ function Form({ responsibility }: FormProps) {
   };
 
   return (
-    <form>
-      <div>
-        <label htmlFor="nameService-input">Nome do serviço</label>
-        <input
-          onChange={ (e) => handleChangeData(e) }
-          name="name"
-          value={ dataLogin.name }
-          id="nameService-input"
-          type="text"
-        />
-      </div>
+    <>
+      <form>
+        <div>
+          <label htmlFor="nameService-input">Nome do serviço</label>
+          <input
+            onChange={ (e) => handleChangeData(e) }
+            name="name"
+            value={ dataLogin.name }
+            id="nameService-input"
+            type="text"
+          />
+        </div>
 
-      <div>
-        <label htmlFor="login-input">Login</label>
-        <input
-          onChange={ (e) => handleChangeData(e) }
-          value={ dataLogin.login }
-          name="login"
-          id="login-input"
-          type="text"
-        />
-      </div>
+        <div>
+          <label htmlFor="login-input">Login</label>
+          <input
+            onChange={ (e) => handleChangeData(e) }
+            value={ dataLogin.login }
+            name="login"
+            id="login-input"
+            type="text"
+          />
+        </div>
 
-      <div>
-        <label htmlFor="password-input">Senha</label>
-        <input
-          onChange={ (e) => handleChangeData(e) }
-          name="password"
-          value={ dataLogin.password }
-          id="password-input"
-          type="password"
-          autoComplete="on"
-        />
-      </div>
+        <div>
+          <label htmlFor="password-input">Senha</label>
+          <input
+            onChange={ (e) => handleChangeData(e) }
+            name="password"
+            value={ dataLogin.password }
+            id="password-input"
+            type="password"
+            autoComplete="on"
+          />
+        </div>
 
-      <div>
-        <label htmlFor="url-input">URL</label>
-        <input
-          onChange={ (e) => handleChangeData(e) }
-          name="url"
-          value={ dataLogin.url }
-          id="url-input"
-          type="text"
-        />
-      </div>
+        <div>
+          <label htmlFor="url-input">URL</label>
+          <input
+            onChange={ (e) => handleChangeData(e) }
+            name="url"
+            value={ dataLogin.url }
+            id="url-input"
+            type="text"
+          />
+        </div>
 
-      <Button responsibility={ log } validBtn={ validBtn }>
-        Cadastrar
-      </Button>
+        <Button responsibility={ log } validBtn={ validBtn }>
+          Cadastrar
+        </Button>
 
-      <Button responsibility={ responsibility }>
-        Cancelar
-      </Button>
-    </form>
+        <Button responsibility={ responsibility }>
+          Cancelar
+        </Button>
+      </form>
+      {validBtn && (
+        <div>
+          <p className={ characters }>Possuir 8 ou mais caracteres</p>
+          <p className={ amountCharacters }>Possuir até 16 caracteres</p>
+          <p className={ numbers }>Possuir letras e números</p>
+          <p className={ special }>Possuir algum caractere especial</p>
+        </div>)}
+    </>
   );
 }
 
--- src/utils/validationLogin.tsx
+++ src/utils/validationLogin.tsx
@@ -4,39 +4,37 @@ type DataProps = {
   password: string;
 };
 
-function validationPassword(senha: string): boolean | string {
+export function validationPassword(senha: string): string {
   // Verifica se a senha está preenchida
   if (!senha) {
-    return 'A senha deve estar preenchida.';
+    return 'characters';
   }
 
   // Verifica o comprimento da senha
   if (senha.length < 8 || senha.length > 16) {
-    return 'Possuir 8 ou mais caracteres';
+    return 'amount characters';
   }
 
   // Verifica se a senha contém letras e números
   if (!senha.match(/[a-zA-Z]/) || !senha.match(/[0-9]/)) {
-    return 'A senha deve conter letras e números.';
+    return 'numbers';
   }
 
   // Verifica se a senha contém algum caractere especial
   if (!senha.match(/[!@#$%^&*(),.?":{}|<>]/)) {
-    return 'A senha deve conter pelo menos um caractere especial.';
+    return 'special';
   }
 
   // Se a senha passou por todas as verificações, é considerada válida
-  return true;
+  return 'true';
 }
 
-const validationData = ({ name, login, password }: DataProps): boolean => {
+export const validationData = ({ name, login, password }: DataProps): boolean => {
   const nameIsTrue = name.length > 0;
   const loginIsTrue = login.length > 0;
   const passwordIsTrue = validationPassword(password);
-  if (nameIsTrue && loginIsTrue && passwordIsTrue === true) {
+  if (nameIsTrue && loginIsTrue && passwordIsTrue === 'true') {
     return nameIsTrue;
   }
   return false;
 };
-
-export default validationData;
